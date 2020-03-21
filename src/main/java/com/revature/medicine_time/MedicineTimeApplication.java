923ef6e8cd1a8424916c4c1036325ea76d4d9e43
gerard-cancino <gerard.cancino@gmail.com>
2020-03-20 22:38:54 -0400

    boot

---
+++ src/main/java/com/revature/medicine_time/MedicineTimeApplication.java
@@ -0,0 +1,13 @@
+package com.revature.medicine_time;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MedicineTimeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MedicineTimeApplication.class, args);
+	}
+
+}
