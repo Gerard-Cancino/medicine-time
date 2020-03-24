882ade95b4d032d89f618579f55c46b43cf7cc86
tasayed <taqrim.sayed@gmail.com>
2020-03-24 15:40:01 -0400

    fixing stuff~

--- src/main/java/com/medicine_time/models/AllMedicine.java
+++ src/main/java/com/medicine_time/models/AllMedicine.java
@@ -5,20 +5,44 @@ import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
-import javax.persistence.Inheritance;
-import javax.persistence.InheritanceType;
 import javax.validation.constraints.NotNull;
 
 @Entity(name="medicine")
-@Inheritance(strategy = InheritanceType.TABLE_PER_CLASS)
-public abstract class AllMedicine {
+public class AllMedicine {
 	@Id
 	@GeneratedValue(strategy=GenerationType.AUTO)
 	@Column
-    protected long id;
+	protected long id;
+
     @NotNull
     @Column(name="name")
 	private String name;
+
 	@Column(name="description")
 	private String description;
+
+	public long getId() {
+		return this.id;
+	}
+
+	public void setId(long id) {
+		this.id = id;
+	}
+
+	public String getName() {
+		return this.name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public String getDescription() {
+		return this.description;
+	}
+
+	public void setDescription(String description) {
+		this.description = description;
+	}
+
 }
