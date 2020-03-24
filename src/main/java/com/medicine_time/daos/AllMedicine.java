d7eef9bc7f1cc435778919a9f61d5cceb700ec39
tasayed <taqrim.sayed@gmail.com>
2020-03-24 15:10:31 -0400

    oh god please work

---
+++ src/main/java/com/medicine_time/daos/AllMedicine.java
@@ -0,0 +1,10 @@
+package main.java.com.medicine_time.daos;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import com.medicine_time.models.AllMedicine;
+
+@Repository
+public interface AllMedicine extends JpaRepository<AllMedicine, Long>{
+}
---
+++ src/main/java/com/medicine_time/services/AllMedicine/AllMedicineImpl.java
@@ -0,0 +1,36 @@
+package main.java.com.medicine_time.services.AllMedicine;
+
+import org.springframework.stereotype.Service;
+
+import com.medicine_time.daos.AllMedicineDao;
+import com.medicine_time.models.AllMedicine;
+
+@Service
+public class AllMedicineServiceImpl implements AllMedicineService{
+
+	private AllMedicineDao am;
+    
+    @Override
+	public AllMedicine viewAllMedicine() {
+		return am.findAll();
+	}
+
+	@Override
+	public AllMedicine getOneMedicine(long id) {
+		return am.getOne(id);
+	}
+
+	@Override
+	@Transactional
+	public AllMedicine updateUser(AllMedicine am) {
+		AllMedicine newAm = am.getOne(am.getId());
+		if(am.getName()!="") {
+			newAm.setName(am.getName());
+		}
+		if(am.getDescription()!="") {
+			newAm.setEmail(am.getEmail());
+		}
+		return newAm;
+	}
+
+}
