cd59cfa76a0c3510488a1d9b8a8a8fc34702eb5b
tasayed <taqrim.sayed@gmail.com>
2020-03-24 16:36:49 -0400

    ~

--- src/main/java/com/medicine_time/services/AllMedicine/AllMedicineService.java
+++ src/main/java/com/medicine_time/services/AllMedicine/AllMedicineService.java
@@ -1,10 +1,11 @@
 package com.medicine_time.services.AllMedicine;
 
 import com.medicine_time.models.AllMedicine;
+import java.util.List;
 
 public interface AllMedicineService {
     //services
-    public AllMedicine viewAllMedicine();
+    public List<AllMedicine> viewAllMedicine();
     public AllMedicine getOneMedicine(long id);
     public AllMedicine updateUser(AllMedicine am);
 }
