ef4491cfb9a4ffbb74f571e875b77cfd5c064afc
tasayed <taqrim.sayed@gmail.com>
2020-03-24 15:49:32 -0400

    .

---
+++ src/main/java/com/medicine_time/controllers/AllMedicineController.java
@@ -0,0 +1,50 @@
+package com.medicine_time.controllers;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.PatchMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.revature.medicine_time.models.AllMedicine;
+import com.revature.medicine_time.services.AllMedicine.AllMedicineService;
+
+@RestController
+@RequestMapping("user")
+@CrossOrigin
+public class AllMedicineController {
+	
+	private AllMedicineService am;
+	
+	@Autowired
+	public AllMedicineController(AllMedicineService am) {
+		this.am = am;
+	}
+
+	@GetMapping("/Medicine/View")
+	@SuppressWarnings("rawtypes")
+	public ResponseEntity viewAllMedicine(){
+		return new ResponseEntity<AllMedicine>(am.viewAllMedicine(),HttpStatus.OK);
+	}
+
+
+	//i dont think this has a link??
+	@PostMapping("/Medicine/View/:ID")
+	@SuppressWarnings("rawtypes")
+	public ResponseEntity getOneMedicine(long id) {
+		return new ResponseEntity<AllMedicine>(am.getOneMedicine(id),HttpStatus.OK);
+	}
+	
+	
+	@PatchMapping
+	@SuppressWarnings("rawtypes")
+	public ResponseEntity addToAllMedicine(AllMedicine am) {
+		if(am.getId()!=0) {
+			return new ResponseEntity<>("Medicine must be an input of 0", HttpStatus.BAD_REQUEST);
+		}
+		return new ResponseEntity<AllMedicine>(us.addToAllMedicine(AllMedicine),HttpStatus.OK);
+	}
+}
