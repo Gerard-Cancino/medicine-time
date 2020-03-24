b2b8de9a243610e95fdd285fd32003191b29474f
tasayed <taqrim.sayed@gmail.com>
2020-03-24 16:59:28 -0400

    ~

--- src/main/java/com/medicine_time/services/AllMedicine/AllMedicineServiceImpl.java
+++ src/main/java/com/medicine_time/services/AllMedicine/AllMedicineServiceImpl.java
@@ -2,11 +2,12 @@ package com.medicine_time.services.AllMedicine;
 
 import org.springframework.stereotype.Service;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.beans.factory.annotation.Transactional;
+import org.springframework.transaction.annotation.Transactional;
 import com.medicine_time.daos.AllMedicineDao;
 import com.medicine_time.models.AllMedicine;
 import com.medicine_time.services.AllMedicineService;
 import java.util.List;
+
 
 @Service
 public class AllMedicineServiceImpl implements AllMedicineService{
